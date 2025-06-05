f3321b91e44247491e258a770d55769713e3e38b
Sebastián Bello <sebastianbello.trejo@gmail.com>
2025-06-05 01:51:34 -0600

    feat: welcome component

---
+++ src/data/welcome.data.ts
@@ -0,0 +1,7 @@
+const data = {
+  title: "Juguetes para la estimulación temprana",
+  description:
+    "Nuestros productos están pensados tanto para los pequeños como para las mamás y papás que desean ofrecer un entorno enriquecedor y lleno de estímulos positivos.",
+};
+
+export default data;
