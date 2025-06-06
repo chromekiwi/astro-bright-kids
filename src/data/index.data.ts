52a689564ae6b059170b831a5267caeb74d12e9e
Sebastián Bello <sebastianbello.trejo@gmail.com>
2025-06-05 21:59:24 -0600

    feat: student component

--- src/data/index.data.ts
+++ src/data/index.data.ts
@@ -1,33 +1,33 @@
 const data = {
-  title: "Astro Basics",
+  title: 'Astro Basics',
   cta: {
-    title: "Contáctanos",
+    title: 'Contáctanos',
     content:
-      "Comparte tus datos con nosotros. Nos pondremos en contacto contigo lo antes posible.",
-    action: "Enviar",
+      'Comparte tus datos con nosotros. Nos pondremos en contacto contigo lo antes posible.',
+    action: 'Enviar',
   },
   nav: [
     {
-      title: "Inicio",
-      link: "/",
+      title: 'Inicio',
+      link: '/',
     },
     {
-      title: "Nosotros",
-      link: "/about",
+      title: 'Nosotros',
+      link: '/about',
     },
   ],
   social_media: [
     {
-      title: "Facebook",
-      link: "https://www.facebook.com",
+      title: 'Facebook',
+      link: 'https://www.facebook.com',
     },
     {
-      title: "X",
-      link: "https://www.x.com",
+      title: 'X',
+      link: 'https://www.x.com',
     },
     {
-      title: "Instagram",
-      link: "https://www.instagram.com",
+      title: 'Instagram',
+      link: 'https://www.instagram.com',
     },
   ],
 };
---
+++ src/data/student.data.ts
@@ -0,0 +1,25 @@
+const data = {
+  title: 'Diseñamos juguetes que inspiran',
+  students: [
+    {
+      name: 'Cynthia',
+    },
+    {
+      name: 'Paula',
+    },
+    {
+      name: 'Romina',
+    },
+    {
+      name: 'Victoria',
+    },
+    {
+      name: 'Diego',
+    },
+    {
+      name: 'Alan',
+    },
+  ],
+};
+
+export default data;
