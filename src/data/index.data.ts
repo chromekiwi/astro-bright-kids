1fbb4558276d23676775e1c531166f039fc563c0
Sebastián Bello <sebastianbello.trejo@gmail.com>
2025-06-06 00:15:37 -0600

    fix: brand identity

--- src/data/index.data.ts
+++ src/data/index.data.ts
@@ -18,16 +18,12 @@ const data = {
   ],
   social_media: [
     {
-      title: 'Facebook',
-      link: 'https://www.facebook.com',
-    },
-    {
-      title: 'X',
-      link: 'https://www.x.com',
-    },
-    {
       title: 'Instagram',
-      link: 'https://www.instagram.com',
+      link: 'https://www.instagram.com/bright_kidsss?igsh=dTVxOTByaXdtOGI1',
+    },
+    {
+      title: 'TikTok',
+      link: 'https://www.tiktok.com/@bright.kids97?_t=ZS-8wxz5pbqDaM&_r=1',
     },
   ],
 };
