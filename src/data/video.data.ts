9ad07b89b4d2ab69787791bdd1811523be17d1b8
Sebastián Bello <sebastianbello.trejo@gmail.com>
2025-06-05 23:58:20 -0600

    feat: video component

---
+++ src/data/video.data.ts
@@ -0,0 +1,25 @@
+const data = {
+  title: 'Nos encanta crear experiencias únicas',
+  videos: [
+    {
+      id: 'nsTlPj8Ea0c',
+      title: 'Conoce nuestros juguetes',
+      description:
+        'Nuestros <strong>rompecabezas</strong> son únicos y permiten a los niños <strong>aprender</strong> mientras <strong>juegan</strong>.',
+    },
+    {
+      id: 'aX-aaq25TLs',
+      title: 'Descubre, aprende y juega',
+      description:
+        'Explora nuestro <strong>kit sensorial</strong> , diseñado para <strong>estimular</strong> los sentidos de los niños.',
+    },
+    {
+      id: 'iUeJJj_rJLU',
+      title: 'Diviértete con nuestros juguetes',
+      description:
+        'Contamos con una amplia variedad de <strong>rompecabezas</strong> para todas las <strong>edades.</strong>',
+    },
+  ],
+};
+
+export default data;
